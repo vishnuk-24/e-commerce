375a6e9a8af8e2b7499faf91a5d2eff25173983a
VishnuK24 <vishnuk@labglo.com>
2020-05-06 22:05:20 +0530

    Removes unnecessary code

--- products/views.py
+++ products/views.py
@@ -6,16 +6,3 @@ from products.models import Product
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
-
-
-# class ProductListView(ListView):
-#     """ProductListView for the list all the prodcts."""
-
-#     context_object_name = 'product_list'
-#     model = Product
-#     template_name = 'product-page.html'
-
-#     def def get_queryset(self):
-#         queryset = super(CLASS_NAME, self).get_queryset()
-#         queryset = queryset # TODO
-#         return queryset
